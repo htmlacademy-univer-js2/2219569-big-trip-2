4f26b9b83573f254555474b29c9e7240bc0b4d7d
Сергей Дубров <tmpvarcher@outlook.com>
2023-04-15 12:15:09 +0500

    another 1.3

--- webpack.config.js
+++ webpack.config.js
@@ -16,15 +16,11 @@ module.exports = {
   ],
   module: {
     rules: [
-      {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        use: ['babel-loader']
-      },
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader']
-      }
+        {
+          test: /\.js$/,
+          exclude: /(node_modules)/,
+          use: ['babel-loader']
+        }
     ]
   }
 };
